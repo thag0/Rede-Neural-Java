72fb1ed731108743c88aa0a2609df47ecb23a86a
Thiago <91092364+thag0@users.noreply.github.com>
2023-08-13 02:30:32 -0300

    Add files via upload

--- rna/avaliacao/Avaliador.java
+++ rna/avaliacao/Avaliador.java
@@ -1,10 +1,12 @@
 package rna.avaliacao;
 
-import rna.Neuronio;
 import rna.RedeNeural;
 
 public class Avaliador{
    AuxiliaresAvaliacao aux;
+
+   EntropiaCruzada entropiaCruzada = new EntropiaCruzada();
+   ErroMedioQuadrado erroMedioQuadrado = new ErroMedioQuadrado();
 
    public Avaliador(){
       this.aux = new AuxiliaresAvaliacao();
@@ -22,18 +24,16 @@ public class Avaliador{
       double[] dadosEntrada = new double[entrada[0].length];
       double[] dadosSaida = new double[saida[0].length];
       double erroMedio = 0;
-      int nSaida = rede.obterCamadaSaida().obterQuantidadeNeuronios();
 
       for(int i = 0; i < entrada.length; i++){//percorrer linhas dos dados
-         //preencher dados de entrada e saída
-         dadosEntrada = entrada[i];
-         dadosSaida = saida[i];
+         System.arraycopy(entrada[i], 0, dadosEntrada, 0, entrada[i].length);
+         System.arraycopy(saida[i], 0, dadosSaida, 0, saida[i].length);
 
          rede.calcularSaida(dadosEntrada);
+         double[] saidaRede = rede.obterSaidas();
 
-         for(int k = 0; k < nSaida; k++){
-            Neuronio neuronio = rede.obterCamadaSaida().neuronios[k];
-            erroMedio += Math.abs(dadosSaida[k] - neuronio.saida);
+         for(int k = 0; k < saidaRede.length; k++){
+            erroMedio += Math.abs(dadosSaida[k] - saidaRede[k]);
          }
       }
 
@@ -85,30 +85,7 @@ public class Avaliador{
     * @return erro médio quadrado da rede em relação ao dados fornecidos (custo/perda).
     */
    public double erroMedioQuadrado(RedeNeural rede, double[][] entrada, double[][] saida){
-      double[] dadosEntrada = new double[entrada[0].length];//tamanho das colunas da entrada
-      double[] dadosSaida = new double[saida[0].length];//tamanho de colunas da saída
-      
-      double diferenca;
-      double custo = 0.0;
-      for(int i = 0; i < entrada.length; i++){//percorrer as linhas da entrada
-         //preencher dados de entrada e saída
-         //método nativo parece ser mais eficiente
-         System.arraycopy(entrada[i], 0, dadosEntrada, 0, entrada[i].length);
-         System.arraycopy(saida[i], 0, dadosSaida, 0, saida[i].length);
-
-         rede.calcularSaida(dadosEntrada);
-         double[] saidaRede = rede.obterSaidas();
-
-         //calcular custo com base na saída
-         for(int j = 0; j < saidaRede.length; j++){
-            diferenca = dadosSaida[j] - saidaRede[j];
-            custo += (diferenca * diferenca);
-         }
-      }
-
-      custo /= entrada.length;
-
-      return custo;
+      return erroMedioQuadrado.calcular(rede, entrada, saida);
    }
 
 
@@ -120,34 +97,6 @@ public class Avaliador{
     * @return entropia cruzada da rede em relação ao dados fornecidos (custo/perda).
     */
    public double entropiaCruzada(RedeNeural rede, double[][] entrada, double[][] saida){  
-      double[] dadosEntrada = new double[entrada[0].length];
-      double[] dadosSaida = new double[saida[0].length];
-  
-      double custo = 0.0;
-      double epsilon = 1e-9;//evitar log 0
-      for(int i = 0; i < entrada.length; i++){//percorrer amostras
-         //preencher dados de entrada e saída
-         //método nativo mais eficiente
-         System.arraycopy(entrada[i], 0, dadosEntrada, 0, entrada[i].length);
-         System.arraycopy(saida[i], 0, dadosSaida, 0, saida[i].length);
-  
-         rede.calcularSaida(dadosEntrada);
-         double[] saidaRede = rede.obterSaidas();
-         
-         double custoExemplo = 0.0;
-         for(int k = 0; k < saidaRede.length; k++){
-            double previsto = saidaRede[k];
-            double real = dadosSaida[k];
-            
-            //fórmula da entropia cruzada para cada neurônio de saída
-            custoExemplo += (-real * Math.log(previsto + epsilon));
-         }
-         
-         custo += custoExemplo;
-      }
-  
-      custo /= entrada.length;
-
-      return custo;
+      return entropiaCruzada.calcular(rede, entrada, saida);
    }
 }
